a0e5b537722860e2a60487760b7f84c6e585de5b
k-ota106 <75551653+k-ota106@users.noreply.github.com>
2022-08-31 23:38:58 +0900

    Fix: installer

--- bin/install_dotfiles.sh
+++ bin/install_dotfiles.sh
@@ -65,7 +65,7 @@ function do_symlink()
     fi
     mkdir -p $tdir
     if [ -e $HOME/$f ];then
-        rm $HOME/$f 
+        rm -rf $HOME/$f 
     fi
     ln -s $this_dir/$f $HOME/$f 
 }
@@ -86,7 +86,7 @@ for f in $(git ls-files);do
         continue
     elif [ $f == ".suggestion" -a -f $HOME/.suggestion ];then
         continue
-    elif [ $f =~ \.md$ ];then
+    elif [[ $f =~ \.md$ ]];then
         continue
     fi
     do_symlink $f
